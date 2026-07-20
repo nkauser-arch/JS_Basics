7cfbbf1d1fcfc3c71bb1903f6ac0c49f8c4b1e49
nkauser-arch <nazkauser7901@gmail.com>
2026-07-20 20:26:20 +0500

    Add files via upload

---
+++ min_max.js
@@ -0,0 +1,22 @@
+const findMinMax = function(arr){
+    let i
+    let largest = arr[0]
+    let smallest = arr[0]
+    for (i of arr){
+        if (i > largest){
+            largest = i;
+        }
+    else if (i < smallest){
+        smallest = i
+        }
+    }
+    return [smallest, largest]
+    //console.log("The smallest value is: ", smallest)
+    //console.log("The smallest value is: ", largest)
+}
+
+result = findMinMax([-10, -5, -20, -1, -30])
+console.log("The smallest value is: ", result[0])
+console.log("The largest value is: ", result[1])
+
+
